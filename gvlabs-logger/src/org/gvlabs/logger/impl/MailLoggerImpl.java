c0a4ca4b5d233d7732559af984b14dd0b840a55b
Thiago Galbiatti Vespa <thiagovespa@gvlabs.org>
2012-12-26 18:53:39 -0200

    Fix: Avoid commented-out lines of code

--- gvlabs-logger/src/org/gvlabs/logger/impl/MailLoggerImpl.java
+++ gvlabs-logger/src/org/gvlabs/logger/impl/MailLoggerImpl.java
@@ -1,12 +1,5 @@
 package org.gvlabs.logger.impl;
 
-//import javax.mail.Message;
-//import javax.mail.MessagingException;
-//import javax.mail.Session;
-//import javax.mail.Transport;
-//import javax.mail.internet.AddressException;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
 
 import org.gvlabs.logger.Logger;
 import org.gvlabs.logger.engine.LoggerLevel;
@@ -24,34 +17,6 @@ public class MailLoggerImpl extends Logger {
 
 	@Override
 	public void log(LoggerLevel level, String msg, Throwable e) {
-//		try {
-//			send("hostname", 25, "joe@smith.com", "sue@smith.com",
-//					"re: dinner", "How about at 7?");
-//		} catch (AddressException e) {
-//			e.printStackTrace();
-//		} catch (MessagingException e) {
-//			e.printStackTrace();
-//		}
-
+		// TODO: Send e-mail
 	}
-
-//	public static void send(String smtpHost, int smtpPort, String from,
-//			String to, String subject, String content) throws AddressException,
-//			MessagingException {
-//		// Create a mail session
-//		java.util.Properties props = new java.util.Properties();
-//		props.put("mail.smtp.host", smtpHost);
-//		props.put("mail.smtp.port", "" + smtpPort);
-//		Session session = Session.getDefaultInstance(props, null);
-//
-//		// Construct the message
-//		Message msg = new MimeMessage(session);
-//		msg.setFrom(new InternetAddress(from));
-//		msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
-//		msg.setSubject(subject);
-//		msg.setText(content);
-//
-//		// Send the message
-//		Transport.send(msg);
-//	}
 }
