2147747d5278f9db9ae681db7f003f2e2595b573
thiagovespa <thiagogv@gmail.com>
2014-06-19 23:50:18 -0300

    Removal of LoggerFactory and avoid cycle between java packages

--- gvlabs-logger/src/org/gvlabs/logger/LoggerInjector.java
+++ gvlabs-logger/src/org/gvlabs/logger/LoggerInjector.java
@@ -1,9 +1,15 @@
 package org.gvlabs.logger;
 
-import static org.gvlabs.logger.impl.LoggerFactory.createLogger;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.logging.Level;
+
+import org.gvlabs.logger.impl.ConsoleLoggerImpl;
+import org.gvlabs.logger.impl.FileLoggerImpl;
+import org.gvlabs.logger.impl.MailLoggerImpl;
 
 public final class LoggerInjector {
 
@@ -100,6 +106,36 @@ public final class LoggerInjector {
 
 		}
 
+		return null;
+	}
+
+	public static Logger createLogger(Annotation ann, LoggerLevel maxLogLevel,
+			Class<?> classToLog, String prefix) {
+		if (ann.annotationType().equals(ConsoleLogger.class)) {
+			ConsoleLogger annInst = (ConsoleLogger) ann;
+			if (annInst.enabled()) {
+				try {
+					return new ConsoleLoggerImpl(maxLogLevel, prefix);
+				} catch (UnsupportedEncodingException e) {
+					JLOGGER.log(Level.SEVERE, "Unsupported Encoding", e);
+				}
+			}
+		} else if (ann.annotationType().equals(FileLogger.class)) {
+			FileLogger annInst = (FileLogger) ann;
+			if (annInst.enabled()) {
+				try {
+					return new FileLoggerImpl(annInst.filePath(), maxLogLevel,
+							prefix);
+				} catch (IOException e) {
+					JLOGGER.log(Level.SEVERE, "Unexpected error", e);
+				}
+			}
+		} else if (ann.annotationType().equals(MailLogger.class)) {
+			MailLogger annInst = (MailLogger) ann;
+			if (annInst.enabled()) {
+				return new MailLoggerImpl(maxLogLevel, prefix);
+			}
+		}
 		return null;
 	}
 }
